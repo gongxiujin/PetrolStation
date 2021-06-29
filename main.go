5b15075574964ff8f602b1a45fce3ba8c0867a0e
gongxiujin <gongxiujin@tencent.com>
2021-06-29 21:01:35 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/someGet", getting)
+	router.POST("/somePost", posting)
+	router.PUT("/somePut", putting)
+	router.DELETE("/someDelete", deleting)
+	router.PATCH("/somePatch", patching)
+	router.HEAD("/someHead", head)
+	router.OPTIONS("/someOptions", options)
+
+	// By default it serves on :8080 unless a
+	// PORT environment variable was defined.
+	router.Run()
+}
